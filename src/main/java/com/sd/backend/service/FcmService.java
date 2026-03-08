f190c8a368d68962f10285543a49811a6cbf470a
Gökhan Aytekin <gokhan.aytekin@idvlabs.com>
2026-03-08 17:34:48 +0300

    feat: Add default sound configurations for Android and APNS notifications.

--- src/main/java/com/sd/backend/service/FcmService.java
+++ src/main/java/com/sd/backend/service/FcmService.java
@@ -1,8 +1,6 @@
 package com.sd.backend.service;
 
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.Notification;
+import com.google.firebase.messaging.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -26,6 +24,16 @@ public class FcmService {
                     .setNotification(Notification.builder()
                             .setTitle(title)
                             .setBody(body)
+                            .build())
+                    .setAndroidConfig(AndroidConfig.builder()
+                            .setNotification(AndroidNotification.builder()
+                                    .setSound("default")
+                                    .build())
+                            .build())
+                    .setApnsConfig(ApnsConfig.builder()
+                            .setAps(Aps.builder()
+                                    .setSound("default")
+                                    .build())
                             .build());
 
             if (data != null && !data.isEmpty()) {
